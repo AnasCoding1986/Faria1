56788b643f9147bbb7cb541c8643de7e81a5cf12
AnasCoding1986 <anassust.scw@gmail.com>
2025-01-30 16:12:16 +0600

    Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
+  base: '/Faria1/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
